d15df72da1bbc2414b06cb99f017b6aa5b1d7432
sonots <sonots@gmail.com>
2014-05-12 20:47:17 +0900

    bundle update

--- heroku.sh
+++ heroku.sh
@@ -1,6 +1,6 @@
 # heroku login
 # heroku destroy fluentd-server
-heroku create fluentd-server
+# heroku create fluentd-server
 git push heroku master
 heroku pg:reset DATABASE --confirm fluentd-server
 heroku run bundle exec fluentd-server init
